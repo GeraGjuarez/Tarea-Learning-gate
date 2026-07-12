98d825b7a6e9ad5543e35ef434c6b3166b69a957
GeraGjuarez <gjuarezl@coflex.com.mx>
2026-07-11 20:02:59 -0600

    Update Gerardo Juarez.py

--- Gerardo Juarez.py
+++ Gerardo Juarez.py
@@ -1,1 +1,7 @@
-print("¡Hola mundo desde GitHub!")
+import streamlit as st
+
+# Cambia el título principal de la página
+st.title("¡Hola Mundo!")
+
+# Puedes agregar un texto explicativo para tu tarea aquí abajo
+st.write("Esta es mi primera página web interactiva creada con Streamlit y Python.")
